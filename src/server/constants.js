8c6f4269c2811455258aa168fc3bfd752d7a3627
Juan Ramollino <juan.ramollino@exxonmobil.com>
2021-08-06 21:32:05 -0300

    Half-way through JWT module.

---
+++ src/server/constants.js
@@ -0,0 +1,7 @@
+module.exports.JWT_OPTIONS = {
+    MEMBER_AUDIENCE: ["SHOW_FAVORITE","LOGIN","SHOW_BOOKS"],
+    ADMIN_AUDIENCE: ["SHOW_FAVORITE,LOGIN","SHOW_BOOKS","ADD_BOOK", "SHOW_USERS"],
+};
+
+module.exports.ADD_BOOK = "ADD_BOOK";
+module.exports.SHOW_USERS = "SHOW_USERS";
--- src/server/server.js
+++ src/server/server.js
@@ -1,69 +1,120 @@
 const express = require("express");
 const cors = require("cors");
 const { uuid } = require("uuidv4");
+const cookieParser = require("cookie-parser");
 const {
   getUserByUsername,
   isEmptyObject,
   isPasswordCorrect,
   getAllBooks,
   getAllUsers,
-  addBook
+  addBook,
+  generateToken,
+  verifyToken,
+  getAudienceFromToken,
+  getFavoriteBooksForUser
 } = require("./shared");
+const Constants = require("./constants");
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-app.use(express.json());
-app.use(cors());
 
-app.get("/users", (req, res) => {
-  // Get all users. Then check if the data contains at least one users, and reurn.
-  // Ohterwise, return a status code of 500 and an empty array.
-  getAllUsers().then( users => {
-    if(users && users.length > 0) res.status(200).send({ users: users })
-    else res.status(500).send({ users: []});
-  })
+app.use(express.json());
+//app.use(cors()); // because we set the proxy variable in the package.json
+app.use(cookieParser());
+
+app.get("/users", verifyToken, (req, res) => {
+  // Check if the user has access to this API
+  if (getAudienceFromToken(req.cookies.token).includes(Constants.SHOW_USERS)) {
+    // Get all users. Then check if the data contains at least one users, and reurn.
+    // Ohterwise, return a status code of 500 and an empty array.
+    getAllUsers().then((users) => {
+      if (users && users.length > 0) {
+        generateToken(req.cookies.token, null,).then((token) => {
+          res.cookie("token", token, { httpOnly: true });
+          res.status(200).send({ users: users })
+        });
+      }
+      else res.status(500).send({ users: [] });
+    })
+  } else res.status(403).send({ message: "Not authorized to view users" });
+
+
 });
 
 // Endpoint for books
-app.get("/books", (req, res) => {
+app.get("/books", verifyToken, (req, res) => {
   // Get all books and check if the data contains at least a book.
   // Otherwise, return an empty array.
   getAllBooks().then(books => {
-    if(books && books.length >0) res.status(200).send({ books: books });
-    else res.status(500).send({ books: []});
+    if (books && books.length > 0) {
+      generateToken(req.cookies.token, null).then(token => {
+        res.cookie("token", token, { httpOnly: true });
+        res.status(200).send({ books: books });
+      });
+    }
+    else res.status(500).send({ books: [] });
   })
 });
 
 app.post("/login", (req, res) => {
   let base64Encoding = req.headers.authorization.split(" ")[1];
   let credentials = Buffer.from(base64Encoding, "base64").toString().split(":");
+
   const username = credentials[0];
   const password = credentials[1];
+
   getUserByUsername(username).then((user) => {
     if (user && !isEmptyObject(user)) {
       isPasswordCorrect(user.key, password).then((result) => {
-        result
-          ? res.status(200).send({ username: user.username, role: user.role })
-          : res
-              .status(401)
-              .send({ message: "username or password is incorrect" });
+        if (!result)
+          res
+            .status(401)
+            .send({ message: "username or password is incorrect" });
+        else {
+          generateToken(null, username).then((token) => {
+            res.cookie("token", token, { httpOnly: true });
+            res.status(200).send({ username: user.username, role: user.role });
+          });
+        }
       });
     } else
       res.status(401).send({ message: "username or password is incorrect" });
   });
 });
 
-app.get("/logout", (req, res) => {});
+app.get("/logout", verifyToken, (req, res) => { 
+  res.clearCookie("token");
+  res.status(200).send({ message: "Cookies cleared"});
+});
 
-app.get("/favorite", (req, res) => {});
-
-app.post("/book", (req, res) => {
-  //console.log(req.body.name);
-  //console.log(req.body.author);
-  addBook({name: req.body.name, author: req.body.author, id: uuid() }).then(
-    err => {
-      if (err) res.status(500).send({ message: "Cannot add this book."});
-      else res.status(200).send({  message: "Book added succesfully. "})
-    }
+app.get("/favorite", verifyToken, (req, res) => {
+  getFavoriteBooksForUser(req.cookies.token).then( books =>
+    generateToken(req.cookies.token, null).then( token => {
+        res.cookie("token", token, { httpOnly: true});
+        res.status(200).send( { favorites: books });
+    })
   );
+});
+
+app.post("/book", verifyToken, (req, res) => {
+  console.log(req.body);
+  if (!req.body.name || !req.body.author) {
+    res.status(400).send({ message: "Invalid book data" });
+  }
+  else {
+    // Check if the user has access to this API
+    if (getAudienceFromToken(req.cookies.token).includes(Constants.ADD_BOOK)) {
+      addBook({ name: req.body.name, author: req.body.author, id: uuid() }).then(
+        err => {
+          if (err) res.status(500).send({ message: "Cannot add this book." });
+          else {
+            generateToken(req.cookies.token, null,).then((token) => {
+              res.cookie("token", token, { httpOnly: true });
+              res.status(200).send({ message: "Book added succesfully" });
+            });
+          }
+        });
+    } else res.status(403).send({ message: "Not authorized to view users" });
+  }
 });
